7881f08da5bb9c58981dc7db4962dd54daad4607
VARvalerie <52732660+VARvalerie@users.noreply.github.com>
2023-05-07 21:50:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -233,17 +233,3 @@ function touchEnded() {
     backgroundCapturedFlag = true;
   }
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
